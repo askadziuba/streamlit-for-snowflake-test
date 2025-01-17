cce8e07cdfcea3a1188a1dc4becc60e87ca32d9c
askadziuba <78427829+askadziuba@users.noreply.github.com>
2025-01-17 10:56:55 +0100

    Create my_app.py

---
+++ my-first-apps/my_app.py
@@ -0,0 +1,72 @@
+import streamlit as st
+import time
+st.title("My first app")
+
+st.text("This is some text.")
+st.code("print('Hello, world!')")
+st.markdown("This is a markdown text.")
+st.latex(r"\LaTeX AbCdeeeeX")
+st.write("This is a write text.")
+
+
+# Sample JSON data
+data = {
+    "name": "Alice",
+    "age": 30,
+    "city": "Wonderland"
+}
+
+# Display the JSON data
+st.json(data)
+
+
+#Create an empty placeholder
+# placeholder = st.empty()
+
+# Update the placeholder with different content
+# for i in range(10):
+#     placeholder.text(f"Iteration {i}")
+#     time.sleep(1)
+
+# Clear the placeholder
+# placeholder.empty()
+
+
+# Create a container
+with st.container():
+    st.write("This is inside the container")
+    st.line_chart([1, 2, 3, 4])
+    st.button("Click me")
+
+# Elements outside the container
+st.write("This is outside the container")
+
+# Create an expander
+with st.expander("See more details"):
+    st.write("Here are some detailed information.")
+    st.line_chart([1, 2, 3, 4])
+    st.button("Expander Click me")
+
+# Elements outside the expander
+st.write("This is outside the expander")
+
+tabs = st.tabs(["1st tab", "2nd tab"])
+with tabs[0]:
+    st.write("Content inside the first tab")
+    # Create a form
+    with st.form("my_form"):
+        st.write("Inside the form")
+
+        # Add input widgets
+        name = st.text_input("Name")
+        age = st.number_input("Age", min_value=0, max_value=100)
+
+        # Add a submit button
+        submitted = st.form_submit_button("Submit")
+
+    # Process the form submission
+    if submitted:
+        st.write(f"Name: {name}")
+        st.write(f"Age: {age}")
+with tabs[1]:
+    st.write("Content inside the second tab")
